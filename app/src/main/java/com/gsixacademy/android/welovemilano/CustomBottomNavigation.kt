c071170d04bfdfe69f99623da1f8cf4059fd45d7
Sajdini <ajdinis1993@gmail.com>
2020-10-10 19:03:15 +0200

    Initial Commit

---
+++ app/src/main/java/com/gsixacademy/android/welovemilano/CustomBottomNavigation.kt
@@ -0,0 +1,18 @@
+package com.gsixacademy.android.welovemilano
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class CustomBottomNavigation: FrameLayout {
+    constructor(context: Context):super(context){
+        init()
+    }
+    constructor (context:Context,attr: AttributeSet?): super(context,attr){
+
+    }
+    constructor (context:Context,attr: AttributeSet?,style:Int): super(context,attr,style){
+
+    }
+    fun init()
+}
